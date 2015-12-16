674d2e8b28a7a039a0e26adcce08f3a02011cad9
Ethan Kerzner <kerzner@sci.utah.edu>
2015-12-16 14:52:48 -0700

    Update find paths plugin and add test3m.

--- plugins/FindPaths.py
+++ plugins/FindPaths.py
@@ -27,8 +27,13 @@ class FindPaths(tlp.Algorithm):
 		sourceLabel = self.dataSet[self.nodeLabel0]
 		targetLabel = self.dataSet[self.nodeLabel2]
 		middleLabel = self.dataSet[self.nodeLabel1]
+		edge0Label = self.dataSet[self.edgeType0]
+		edge1Label = self.dataSet[self.edgeType1]
+		viewSelection = self.graph.getBooleanProperty("viewSelection")
+		linkedStructures = self.graph.getProperty("LinkedStructures")
+		
 		# Get all nodes in the starting label
-		print 'Setting up source nodes'
+		print 'Searching for source nodes'
 		print '    searching for source nodes with label: ' + sourceLabel
 		sources = tp.utils.getNodesByType(sourceLabel, self.graph)
 		print '    found ' + str(len(sources)) + ' nodes with label ' + sourceLabel
@@ -42,17 +47,30 @@ class FindPaths(tlp.Algorithm):
 		print ''
 		
 		# Find paths
-	
 		print 'Finding paths'
+		print sourceLabel + ', ' + edge0Label + ', ' + middleLabel + ', ' + edge1Label + ', ' + targetLabel
 		for source in sources:
 			for target in targets:
 				pathFinder = tp.PathFinder(self.graph)
-				pathFinder.findPaths(source, target, 3)
+				pathFinder.findPaths(source, target, 2)
 				for path in pathFinder.valid:
+					print 'testing path'
+					print path.toStringOfTypes()
 					if tp.utils.getNodeType(path.nodes[1], self.graph) == middleLabel:
-						print '    from node ' + str(tp.utils.getNodeId(source, self.graph)) + ' the paths to ' + str(tp.utils.getNodeId(target, self.graph)) + ' are:'	
-						print '        ' + path.toStringOfTypes()
-		
+						for node in path.nodes:
+							viewSelection[node] = True
+						for edge in path.edges:
+							viewSelection[edge] = True
+						startNodeId = str(tp.utils.getNodeId(path.nodes[0], self.graph))
+						startEdgeStructures = linkedStructures[path.edges[0]].lstrip(' ')
+						middleNodeId = str(tp.utils.getNodeId(path.nodes[1], self.graph))
+						endEdgeStructures = linkedStructures[path.edges[1]].lstrip(' ')
+						endNodeId = str(tp.utils.getNodeId(path.nodes[2], self.graph))
+						print startNodeId + ', (' + startEdgeStructures + '), ' + middleNodeId + ', (' + endEdgeStructures + '), ' + endNodeId
+						
+		print ''
+		print 'Statistics'
+	
 		# Print those paths
 		return True
 
