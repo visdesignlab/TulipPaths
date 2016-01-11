67506fb92936fb00f2ca2ff5b4fc12ff7144cd38
kerzner <ethan.kerzner@gmail.com>
2016-01-11 11:06:02 -0700

    Update feedback stats.

--- experiments/feedbackStats.py
+++ experiments/feedbackStats.py
@@ -38,12 +38,18 @@ numNeither = 0
 numNotSkipped = 0
 
 gcNodesToCBbNodes = {}
+numAcsInMotif = {}
 ffNodes = tp.utils.getNodesByTypes(ffNodeTypes, graph)
 fbNodes = tp.utils.getNodesByTypes(fbNodeTypes, graph)
 for node in ffNodes:
     gcNodesToCBbNodes[node] = {}
+    numAcsInMotif[node] = {}
     for nodeType in fbNodeTypes:
         gcNodesToCBbNodes[node][nodeType] = []
+        numAcsInMotif[node][nodeType] = []
+
+markedNodes = []
+markedEdges = []
 
 for node in acNodes:
 
@@ -55,6 +61,16 @@ for node in acNodes:
     isFeedbackNode = (len(inputFromCBb) > 0) and (len(outputToFb) > 0)
     isFeedForwardNode = (len(inputFromCBb) > 0) and (len(outputToFf) > 0)
 
+    if isFeedbackNode or isFeedForwardNode:
+        for results in [outputToFb, outputToFf, inputFromCBb]:
+            for path in results:
+                for temp in path.nodes:
+                    if temp not in markedNodes:
+                        markedNodes.append(temp)
+                for temp in path.edges:
+                    if temp not in markedEdges:
+                        markedEdges.append(temp)
+
     id = tp.utils.getNodeId(node, graph)
     nodeType = tp.utils.getNodeType(node, graph)
 
@@ -62,7 +78,6 @@ for node in acNodes:
         outputCountsStr += id + ', ' + nodeType + ', ' + str(isFeedbackNode) + ', ' + str(isFeedForwardNode) + ', ' + str(len(inputFromCBb)) + ', ' + str(len(outputToFb)) + ', ' + str(len(outputToFf)) + ', ' + str(completeness[node]) + '\n'
     else:
         numNeither += 1
-
 
     localPathsStr = ''
 
@@ -94,35 +109,105 @@ for node in acNodes:
     for path in outputToFf:
         gcNode = path.nodes[1]
         print 'gc nodes type is' + tp.utils.getNodeType(gcNode, graph)
+
         for startPath in inputFromCBb:
             cbbNode = startPath.nodes[0]
             nodeType = tp.utils.getNodeType(cbbNode, graph)
+            if node not in numAcsInMotif[gcNode][nodeType]:
+                numAcsInMotif[gcNode][nodeType].append(node)
             gcNodesToCBbNodes[gcNode][nodeType].append(cbbNode)
+
 
 print gcNodesToCBbNodes
 
-stats = open('connectivity_matrix.csv', 'w')
-stats.write('nodeid, node label,')
-for nodeType in fbNodeTypes:
+ids = [606 ,6857 ,15796 ,5107 ,8575 ,74047 ,53399 ,53436 ,54695 ,54701 ,52882 ,8580 ,70924 ,64887 ,64923 ,7594 ,31024 ,29198 ,68539 ,16087, 5118]
+desiredLabels = ["CBb3m", "CBb5w", "CBb4w", "CBb3-4i", "CBb5i", "CBb5-6i", "CBb4i", "CBb3-4-5i", "CBb3", "CBb4-5-6i", "CBb3i"]
+stats = open('connectivity_matrix_counting_motifs.csv', 'w')
+stats.write('node id, node label')
+for nodeType in desiredLabels:
     stats.write(', ' + nodeType)
 stats.write('\n')
-for node in gcNodesToCBbNodes.keys():
 
+for id in ids:
+    node = tp.utils.getNodeById(id, graph)
     check = 0
-    for nodeType in fbNodeTypes:
+    for nodeType in desiredLabels:
+        check += len(numAcsInMotif[node][nodeType])
+
+    if check == 0:
+        print ' skipping node ' + tp.utils.getNodeId(node, graph) + ', ' + tp.utils.getNodeType(node, graph)
+        continue
+    stats.write(tp.utils.getNodeId(node, graph) + ', ' + tp.utils.getNodeType(node, graph) + ', ')
+    for nodeType in desiredLabels:
+        stats.write(str(len(numAcsInMotif[node][nodeType])) + ', ')
+    stats.write('\n')
+stats.close()
+
+stats = open('connectivity_matrix.csv', 'w')
+stats.write('nodeid, node label')
+for nodeType in desiredLabels:
+    stats.write(', ' + nodeType)
+stats.write('\n')
+for id in ids:
+    node = tp.utils.getNodeById(id, graph)
+    for nodeType in desiredLabels:
         check += len(gcNodesToCBbNodes[node][nodeType])
 
     if check == 0:
         print ' skipping node ' + tp.utils.getNodeId(node, graph) + ', ' + tp.utils.getNodeType(node, graph)
         continue
 
-
     stats.write(tp.utils.getNodeId(node, graph) + ', ' + tp.utils.getNodeType(node, graph) + ', ')
-    for nodeType in fbNodeTypes:
+    for nodeType in desiredLabels:
         stats.write(str(len(gcNodesToCBbNodes[node][nodeType])) + ', ')
     stats.write('\n')
 stats.close()
 
+stats = open('connectivity_matrix_count_unique_cbbs.csv', 'w')
+stats.write('nodeid, node label')
+for nodeType in desiredLabels:
+    stats.write(', ' + nodeType)
+stats.write('\n')
+for id in ids:
+    node = tp.utils.getNodeById(id, graph)
+    check = 0
+    for nodeType in desiredLabels:
+        check += len(gcNodesToCBbNodes[node][nodeType])
+
+    if check == 0:
+        print ' skipping node ' + tp.utils.getNodeId(node, graph) + ', ' + tp.utils.getNodeType(node, graph)
+        continue
+
+    stats.write(tp.utils.getNodeId(node, graph) + ', ' + tp.utils.getNodeType(node, graph) + ', ')
+    for nodeType in desiredLabels:
+        nodeSet = set(gcNodesToCBbNodes[node][nodeType])
+        stats.write(str(len(nodeSet)) + ', ')
+    stats.write('\n')
+stats.close()
+
+acUsage = {}
+
+for id in ids:
+    node = tp.utils.getNodeById(id, graph)
+    for nodeType in desiredLabels:
+        acs = numAcsInMotif[node][nodeType]
+        for ac in acs:
+            if acUsage.has_key(ac):
+                acUsage[ac].append(id)
+            else:
+                acUsage[ac] = [id]
+
+stats = open('ac_usage.csv', 'w')
+stats.write('ac id, ac label, num usage, usage string\n')
+for key in acUsage.keys():
+    id = tp.utils.getNodeId(key, graph)
+    type = tp.utils.getNodeType(key, graph)
+    listOfGcs = ''
+    for gc in acUsage[key]:
+        listOfGcs += str(gc) + ';'
+
+    stats.write(str(id) + ', ' +  type + ', ' + str(len(acUsage[key])) + ', ' + listOfGcs + '\n')
+stats.close()
 
 
 stats = open('stats.csv', 'w')
@@ -145,6 +230,16 @@ stats.close()
 print 'percent neither ff nor fb: '  + str(numNeither / float(numNotSkipped))
 
 # Render the graph in a node-link diagram.
+
+for node in graph.getNodes():
+    if node not in markedNodes:
+        graph.delNode(node)
+
+for edge in graph.getEdges():
+    if edge not in markedEdges:
+        graph.delEdge(edge)
+
+tlp.saveGraph(graph, 'shit.tlp')
 nodeLinkView = tlpgui.createNodeLinkDiagramView(graph)
 
 
