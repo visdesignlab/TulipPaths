236a67dc516fd59f1cf648bdc3a6a04e98856258
Ethan Kerzner <ethan.kerzner@gmail.com>
2016-02-19 13:19:47 -0700

    add attributes to exporter.

--- tulippaths/export.py
+++ tulippaths/export.py
@@ -1,12 +1,118 @@
 import tulippaths as tp
 
+def getEdgeAttributes():
+    edgeAttributes = []
+    edgeAttributes.append({
+        "Name": "ID",
+        "DisplayName": "id",
+        "Type": "int",
+        "DataType": "index",
+        "Unique": "true"
+    })
+
+    edgeAttributes.append({
+        "Name": "SourceID",
+        "DisplayName": "source id",
+        "Type": "int",
+        "DataType": "source-index",
+        "Unique": "false"
+    })
+
+    edgeAttributes.append({
+        "Name": "TargetID",
+        "DisplayName": "target id",
+        "Type": "int",
+        "DataType": "target-index",
+        "Unique": "true"
+    })
+
+    edgeAttributes.append({
+        "Name": "TargetID",
+        "DisplayName": "target id",
+        "Type": "int",
+        "DataType": "target-index",
+        "Unique": "true"
+    })
+
+    edgeAttributes.append({
+        "Name": "Type",
+        "DisplayName": "edge type",
+        "Type": "string",
+        "DataType": "categorical",
+        "Unique": "false"
+    })
+
+    edgeAttributes.append({
+        "Name": "LinkedStructures",
+        "DisplayName": "structures",
+        "Type": "string",
+        "DataType": "string",
+        "Unique": "false"
+    })
+
+    return edgeAttributes
+
+
+def getNodeAttributes():
+    nodeAttributes = []
+
+    nodeAttributes.append({
+        "Name": "ID",
+        "DisplayName": "id",
+        "Type": "int",
+        "DataType": "index",
+        "Unique": "true"
+    })
+
+    nodeAttributes.append({
+        "Name": "Label",
+        "DisplayName": "label",
+        "Type": "string",
+        "DataType": "categorical",
+        "Unique": "false"
+    })
+
+    nodeAttributes.append({
+        "Name": "StructureID",
+        "DisplayName": "structure",
+        "Type": "int",
+        "DataType": "id",
+        "Unique": "true"
+    })
+
+    nodeAttributes.append({
+        "Name": "Completeness",
+        "DisplayName": "completeness",
+        "Type": "float",
+        "DataType": "quantitative",
+        "Unique": "false"
+    })
+
+    nodeAttributes.append({
+        "Name": "HullArea",
+        "DisplayName": "area",
+        "Type": "float",
+        "DataType": "quantitative",
+        "Unique": "false"
+    })
+
+    nodeAttributes.append({
+        "Name": "Locations",
+        "DisplayName": "locations",
+        "Type": "int",
+        "DataType": "quantitative",
+        "Unique": "false"
+    })
+
+    return nodeAttributes
+
 
 def graphToJson(graph):
     completeness = tp.utils.getApproximateAnnotationCompleteness(graph)
     nodes = []
     edges = []
     hullArea = graph.getDoubleProperty("hull area")
-    locations = graph.getDoubleProperty("hull area")
+    locations = graph.getDoubleProperty("locations")
     for node in graph.getNodes():
         dictionary = {
             "ID": int(node.id),
@@ -32,7 +138,12 @@ def graphToJson(graph):
         }
         edges.append(dictionary)
 
+    nodeAttributes = getNodeAttributes()
+    edgeAttributes = getEdgeAttributes()
+
     return {
+        "nodeAttributes": nodeAttributes,
         "nodes": nodes,
+        "edgeAttributes": edgeAttributes,
         "edges": edges
     }
