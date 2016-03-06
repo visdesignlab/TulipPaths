88c4bc578c60fa7d19c5be5339f2054cd4387be3
Nathaniel Nelson <nelson.nleroy@gmail.com>
2016-03-06 14:25:11 -0700

    Mac plugin install script creates plugin directory if necessary

--- plugins/install-mac.sh
+++ plugins/install-mac.sh
@@ -3,10 +3,15 @@
 # Obtain a list of TulipPath plugin files
 FILES=./*.py
 
-# TODO Make sure a directory exists for python plugins
-
 HOME_DIR=$(eval echo '~')
 TARGET_DIR="${HOME_DIR}/.Tulip-4.8/plugins/python"
+
+# Make sure a directory exists for python plugins
+if [ ! -d $TARGET_DIR ]; then
+    mkdir "${HOME_DIR}/.Tulip-4.8/"
+    mkdir "${HOME_DIR}/.Tulip-4.8/plugins/"
+    mkdir "${HOME_DIR}/.Tulip-4.8/plugins/python/"
+fi
 
 # Copy each plugin script to the python plugin directory
 for file in $FILES
