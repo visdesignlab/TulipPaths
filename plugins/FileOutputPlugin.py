1e26cd9d49eed9383b580d45998c47ed8281c5ab
Nathaniel Nelson <nelson.nleroy@gmail.com>
2016-03-09 19:46:49 -0700

    FileOutputPlugin doesn't need to stub algorithm methods

--- plugins/FileOutputPlugin.py
+++ plugins/FileOutputPlugin.py
@@ -20,13 +20,6 @@ class FileOutputPlugin(tlp.Algorithm):
         # the file we just created
         self.addStringParameter(self._outputFileLabel, "", self._outputFilepath)
 
-    def check(self):
-        return True
-
-    def run(self):
-        return True
-
-
     def outputFilepath(self, filename):
         """ Return the full filepath of a file with the given name,
         placing it in the home directory
