37d6b99214176fc41232536917c1e5a3caae5c9f
Ethan Kerzner <ethan.kerzner@gmail.com>
2016-12-15 11:26:36 -0700

    Remove experiments

--- experiments/scratch.py
+++
@@ -1,9 +0,0 @@
-from tulip import *
-import tulippaths as tp
-
-graph = tlp.loadGraph("../data/514_10hops.tlp")
-
-sources = tp.utils.getNodesByTypeRegex("CBb4w", graph)
-
-for source in sources:
-    print tp.utils.getNodeType(source, graph)
